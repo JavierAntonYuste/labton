f9a5c54f1725e8da1aba9c299fc91ed9e4c87192
JavierAntonYuste <36293800+JavierAntonYuste@users.noreply.github.com>
2019-11-18 11:20:36 +0100

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -2,7 +2,7 @@ import os
 
 #SECRET_KEY = 'p9Bv<3Eid9%$i01'
 SECRET_KEY = os.urandom(12)
-SQLALCHEMY_DATABASE_URI = 'mysql://javi-anton:Janyu97@db/tfg_db'
+SQLALCHEMY_DATABASE_URI = 'Your DB URI'
 
 # Create in-memory database
 SQLALCHEMY_ECHO = True
